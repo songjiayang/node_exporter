64726389fcc8042a1bf8d97cdffc45f557026da2
songjiayang <songjiayang1@gmail.com>
2016-07-26 18:14:45 +0800

    strengthen supervisord collector, enable to collect running process's CPU and memory information

--- collector/supervisord.go
+++ collector/supervisord.go
@@ -21,6 +21,7 @@ import (
 	"github.com/kolo/xmlrpc"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/common/log"
+	"github.com/shirou/gopsutil/process"
 )
 
 var (
@@ -33,6 +34,8 @@ type supervisordCollector struct {
 	stateDesc      *prometheus.Desc
 	exitStatusDesc *prometheus.Desc
 	uptimeDesc     *prometheus.Desc
+	cpu            *prometheus.CounterVec
+	memory         *prometheus.Desc
 }
 
 func init() {
@@ -74,6 +77,21 @@ func NewSupervisordCollector() (Collector, error) {
 			prometheus.BuildFQName(Namespace, subsystem, "uptime"),
 			"Process Uptime",
 			labelNames,
+			nil,
+		),
+		cpu: prometheus.NewCounterVec(
+			prometheus.CounterOpts{
+				Namespace: Namespace,
+				Subsystem: subsystem,
+				Name:      "process_cpu",
+				Help:      "Seconds the CPU spent in each mode.",
+			},
+			[]string{"name", "group", "mode"},
+		),
+		memory: prometheus.NewDesc(
+			prometheus.BuildFQName(Namespace, subsystem, "process_memory"),
+			"Memory used information by mode",
+			[]string{"name", "group", "mode"},
 			nil,
 		),
 	}, nil
@@ -125,6 +143,7 @@ func (c *supervisordCollector) Update(ch chan<- prometheus.Metric) error {
 		if c.isRunning(info.State) {
 			ch <- prometheus.MustNewConstMetric(c.upDesc, prometheus.GaugeValue, 1, lables...)
 			ch <- prometheus.MustNewConstMetric(c.uptimeDesc, prometheus.CounterValue, float64(info.Now-info.Start), lables...)
+			c.CollectProcessCPUAndMemory(ch, info.PID, info.Name, info.Group)
 		} else {
 			ch <- prometheus.MustNewConstMetric(c.upDesc, prometheus.GaugeValue, 0, lables...)
 			ch <- prometheus.MustNewConstMetric(c.uptimeDesc, prometheus.CounterValue, 0, lables...)
@@ -133,4 +152,75 @@ func (c *supervisordCollector) Update(ch chan<- prometheus.Metric) error {
 	}
 
 	return nil
+}
+
+func (c *supervisordCollector) CollectProcessCPUAndMemory(ch chan<- prometheus.Metric, pid int, pname, pgroup string) (err error) {
+	p, err := process.NewProcess(int32(pid))
+	if err != nil {
+		return
+	}
+
+	timesStat, err := p.Times()
+	if err != nil {
+		return
+	}
+
+	memInfo, err := p.MemoryInfo()
+	if err != nil {
+		return
+	}
+
+	children, err := p.Children()
+
+	// if process without children, err will not be nil.
+	if err == nil && len(children) > 0 {
+		for _, child := range children {
+			ctStat, err := child.Times()
+			if err != nil {
+				return err
+			}
+
+			timesStat.User += ctStat.User
+			timesStat.System += ctStat.System
+			timesStat.Idle += ctStat.Idle
+			timesStat.Nice += ctStat.Nice
+			timesStat.Iowait += ctStat.Iowait
+			timesStat.Irq += ctStat.Irq
+			timesStat.Softirq += ctStat.Softirq
+			timesStat.Steal += ctStat.Steal
+			timesStat.Guest += ctStat.Guest
+			timesStat.GuestNice += ctStat.GuestNice
+			timesStat.Stolen += ctStat.Stolen
+
+			cmem, err := child.MemoryInfo()
+			if err != nil {
+				return err
+			}
+
+			memInfo.RSS += cmem.RSS
+			memInfo.VMS += cmem.VMS
+			memInfo.Swap += cmem.Swap
+		}
+	}
+
+	// collect cpu information
+	c.cpu.With(prometheus.Labels{"name": pname, "group": pgroup, "mode": "user"}).Set(timesStat.User)
+	c.cpu.With(prometheus.Labels{"name": pname, "group": pgroup, "mode": "system"}).Set(timesStat.System)
+	c.cpu.With(prometheus.Labels{"name": pname, "group": pgroup, "mode": "idle"}).Set(timesStat.Idle)
+	c.cpu.With(prometheus.Labels{"name": pname, "group": pgroup, "mode": "nice"}).Set(timesStat.Nice)
+	c.cpu.With(prometheus.Labels{"name": pname, "group": pgroup, "mode": "iowait"}).Set(timesStat.Iowait)
+	c.cpu.With(prometheus.Labels{"name": pname, "group": pgroup, "mode": "irq"}).Set(timesStat.Irq)
+	c.cpu.With(prometheus.Labels{"name": pname, "group": pgroup, "mode": "softirq"}).Set(timesStat.Softirq)
+	c.cpu.With(prometheus.Labels{"name": pname, "group": pgroup, "mode": "steal"}).Set(timesStat.Steal)
+	c.cpu.With(prometheus.Labels{"name": pname, "group": pgroup, "mode": "guest"}).Set(timesStat.Guest)
+	c.cpu.With(prometheus.Labels{"name": pname, "group": pgroup, "mode": "guestNice"}).Set(timesStat.GuestNice)
+	c.cpu.With(prometheus.Labels{"name": pname, "group": pgroup, "mode": "stolen"}).Set(timesStat.Stolen)
+	c.cpu.Collect(ch)
+
+	// collect the memory used information
+	ch <- prometheus.MustNewConstMetric(c.memory, prometheus.GaugeValue, float64(memInfo.RSS), []string{pname, pgroup, "rss"}...)
+	ch <- prometheus.MustNewConstMetric(c.memory, prometheus.GaugeValue, float64(memInfo.VMS), []string{pname, pgroup, "vms"}...)
+	ch <- prometheus.MustNewConstMetric(c.memory, prometheus.GaugeValue, float64(memInfo.Swap), []string{pname, pgroup, "swap"}...)
+
+	return
 }
